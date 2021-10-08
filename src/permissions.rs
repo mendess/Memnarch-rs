427cb74a428ee890e49548c46270842a5e8fe946
mendess <pedro.mendes.26@gmail.com>
2021-10-08 01:30:39 +0100

    I am now my friend

--- src/permissions.rs
+++ src/permissions.rs
@@ -7,13 +7,21 @@ use serenity::{
 #[check]
 #[name = "is_friend"]
 pub async fn is_friend(
-    _: &Context,
+    ctx: &Context,
     msg: &Message,
     _: &mut Args,
     _: &CommandOptions,
 ) -> Result<(), Reason> {
+    let owner = ctx
+        .http
+        .get_current_application_info()
+        .await
+        .map(|info| info.owner.id)
+        .ok();
+
     msg.guild_id
         .and_then(|id| (id.0 == 136_220_994_812_641_280).then(|| Ok(())))
+        .or_else(|| (Some(msg.author.id) == owner).then(|| Ok(())))
         .unwrap_or_else(|| {
             Err(Reason::User(
                 "You don't have permission to use that command!".to_string(),
